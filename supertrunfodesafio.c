388a060bba46d701db4aaf7130f555c188b063c0
Naymaral <naymaras@gmail.com>
2025-04-14 12:51:41 -0300

    Add files via upload

---
+++ supertrunfodesafio.c
@@ -0,0 +1,163 @@
+#include <stdio.h> 
+
+int main (){
+
+    printf("DESAFIO SUPER TRUNFO INICIANTE");
+
+    printf("\n");
+
+
+    //V.CIDADE01:
+
+    char estado1[90];
+    int codigo_estado1;
+    char cidade1[100];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos_turisticos1;
+
+    //carta01:
+
+    //entrada de dados_1:
+
+    printf("CADASTRE SUA 1º CARTA(01)! \n");
+    printf("\n");
+
+
+    printf("Digite o nome do Estado: \n");
+    scanf("%s", &estado1);
+    printf("\n");
+
+    printf("Digite o codigo do Estado: \n");
+    scanf("%d", &codigo_estado1);
+    printf("\n");
+    
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", &cidade1);
+    printf("\n");
+
+    printf("Total de Habitantes: \n");
+    scanf("%d", &populacao1);
+    printf("\n");
+
+    printf("Area: \n");
+    scanf("%f", &area1);
+    printf("\n");
+
+    printf("Pib: \n");
+    scanf("%f", &pib1);
+    printf("\n");
+
+    printf("Pontos Turisticos: \n");
+    scanf("%d", &pontos_turisticos1);
+    printf("\n");
+
+    //saida de dados_1;
+
+    printf("CARTA 01 \n");
+    printf("\n");
+    //
+    printf("Estado: %s \n", estado1);
+    printf("\n");
+
+    printf("Codigo do Estado: %d \n", codigo_estado1);
+    printf("\n");
+    //
+    printf("Cidade: %s \n", cidade1);
+    printf("\n");
+    //
+    printf("Habitantes: %d \n", populacao1);
+    printf("\n");
+    //
+    printf("Area: %f \n", area1);
+    printf("\n");
+    //
+    printf("Pib: %f \n", pib1);
+    printf("\n");
+    //
+    printf("Pontos Turisticos: %d \n", pontos_turisticos1);
+    printf("\n");
+
+    printf("CARTA (01) CADASTRADA!\n");
+    printf("\n");
+    //
+    //
+    //V.CIDADE02:
+
+    char estado2[90];
+    int codigo_estado2;
+    char cidade2[100];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos_turisticos2;
+
+    //carta02:
+
+    //entrada de dados_2:
+
+    printf("CADASTRE SUA 2º CARTA(01)! \n");
+    printf("\n");
+
+    printf("Digite o nome do Estado: \n");
+    scanf("%s", &estado2);
+    printf("\n");
+
+    printf("Digite o codigo do Estado: \n");
+    scanf("%d", &codigo_estado2);
+    printf("\n");
+    
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", &cidade2);
+    printf("\n");
+
+    printf("Total de Habitantes: \n");
+    scanf("%d", &populacao2);
+    printf("\n");
+
+    printf("Area: \n");
+    scanf("%f", &area2);
+    printf("\n");
+
+    printf("Pib: \n");
+    scanf("%f", &pib2);
+    printf("\n");
+
+    printf("Pontos Turisticos: \n");
+    scanf("%d", &pontos_turisticos2);
+    printf("\n");
+
+    //saida de dados_2;
+
+    printf("CARTA 02 \n");
+    printf("\n");
+    //
+    printf("Estado: %s \n", estado2);
+    printf("\n");
+
+    printf("Codigo do Estado: %d \n", codigo_estado2);
+    printf("\n");
+    //
+    printf("Cidade: %s \n", cidade2);
+    printf("\n");
+    //
+    printf("Habitantes: %d \n", populacao2);
+    printf("\n");
+    //
+    printf("Area: %f \n", area2);
+    printf("\n");
+    //
+    printf("Pib: %f \n", pib2);
+    printf("\n");
+    //
+    printf("Pontos Turisticos: %d \n", pontos_turisticos2);
+    printf("\n");
+
+    printf("CARTA (02) CADASTRADA!\n");
+    printf("\n");
+
+
+    return 0;
+
+}
